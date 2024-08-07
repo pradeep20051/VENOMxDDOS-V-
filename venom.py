85ca04fa0b9629c10d630cac99fa3927bbfd7dd6
pradeep20051 <pubghack852138@gmail.com>
2024-08-07 22:02:44 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,10 +4,10 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '6775242061:AAFzhh8pOomG6qHCd-lY0QV763urQ7zhueA'
+ADMIN_ID = '5816432312'
 MAX_RESTARTS = 5
-RESTART_PERIOD = 60  # Seconds
+RESTART_PERIOD = 10  # Seconds
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 bot = Bot(API_TOKEN)
